0630c9c218ca6e265b55f93e226d0cd22f4e4ef2
patriot612 <rm9r6tkvr8@privaterelay.appleid.com>
2026-09-23 23:02:50 +0400

    Delete src/telegram/markup.ts

--- src/telegram/markup.ts
+++
@@ -1,6 +0,0 @@
-export type InlineButton={text:string;callback_data:string};
-export type InlineKeyboard=InlineButton[][];
-export type ReplyKeyboard={keyboard:string[][];resize_keyboard?:boolean;is_persistent?:boolean;one_time_keyboard?:boolean};
-
-export const ik=(rows:InlineButton[][])=>({inline_keyboard:rows});
-export const rk=(rows:string[][]):ReplyKeyboard=>({keyboard:rows,resize_keyboard:true,is_persistent:true});
