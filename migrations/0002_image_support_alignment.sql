77bd9b3a1d87ea0fd9b320773e04a29f7cdb04a7
patriot612 <rm9r6tkvr8@privaterelay.appleid.com>
2026-09-24 20:34:05 +0400

    Delete migrations/0002_image_support_alignment.sql

--- migrations/0002_image_support_alignment.sql
+++
@@ -1,8 +0,0 @@
--- Align image feature gating and image-model metadata for existing installations.
-INSERT INTO settings(key,value,updated_at)
-VALUES('feature_image','1',datetime('now'))
-ON CONFLICT(key) DO NOTHING;
-
-UPDATE models
-SET supports_images=1
-WHERE type='image';
