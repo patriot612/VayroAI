95dad490277b3076f135b0dc4596e60a05df9438
patriot612 <rm9r6tkvr8@privaterelay.appleid.com>
2026-09-24 20:34:12 +0400

    Delete migrations/0003_hardening.sql

--- migrations/0003_hardening.sql
+++
@@ -1,6 +0,0 @@
--- VayroAI hardening: indexes for fair FIFO image queues and cleanup.
-CREATE INDEX IF NOT EXISTS idx_image_generations_user_status_created
-  ON image_generations(user_id,status,created_at);
-
-CREATE INDEX IF NOT EXISTS idx_image_generations_status_created
-  ON image_generations(status,created_at);
